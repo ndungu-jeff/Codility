4775aa00ff24d3e2a49eb0110d00c6520ef3b1b8
jeff ndungu <ndungujeff.ke@gmail.com>
2023-03-27 09:33:06 +0300

    DOne

--- index.rb
+++ index.rb
@@ -1,5 +1,10 @@
 class String
-
+  # merging overlapping strings
+  # split the string and map in to it
+  # use the join method to join the array
+  # use the capitalize method to loop the array
+    
+  end
     def camelCase
       split.map(&:capitalize).join
     end
